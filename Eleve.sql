6324df6178949945ea4b73e534cbaae1318a9fa0
LeilaSmh <leilasamih.ls@gmail.com>
2020-12-22 18:02:58 +0100

    Before Last

---
+++ Eleve.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Eleve]
+(
+	[ID_Eleve] INT NOT NULL , 
+    [CNE] NCHAR(10) NOT NULL, 
+    [Nom] NCHAR(20) NOT NULL, 
+    [Prenom] VARCHAR(30) NOT NULL, 
+    [Photo] IMAGE NULL, 
+    [Tel] NCHAR(25) NOT NULL, 
+    [Email] NCHAR(30) NOT NULL, 
+    PRIMARY KEY ([ID_Eleve],[CNE])
+)
---
+++ WcfServiceEnseignement/ServiceEnseignement.svc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+namespace WcfServiceEnseignement
+{
+ public class ServiceEnseignement : IServiceEnseignement
+    {
+        DataClassesEnseignementDataContext l = new DataClassesEnseignementDataContext();
+        public List<Eleve> getByCNE(string s)
+        {
+            return (from p in l.Eleves where p.CNE == s select p).ToList();
+        }
+    }
+}
